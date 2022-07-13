b44ccd6900831e100504ad165b425266b537b56b
Anton Akulov <antonakv@users.noreply.github.com>
2022-07-13 10:40:39 +0200

    Adding run terraform script

--- scripts/install_kitchen.sh
+++ scripts/install_kitchen.sh
@@ -1,14 +1,25 @@
 #!/usr/bin/env bash
 
 export DEBIAN_FRONTEND=noninteractive
-apt-get update
-apt-get install -y rbenv ruby-build
 
-# Installing ruby
-rbenv install -l
-rbenv install 2.6.6
-rbenv local 2.6.6
+sudo apt-get -y install gnupg2
+
+gpg2 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+
+\curl -sSL https://get.rvm.io | bash -s stable
+
+sudo usermod -a -G rvm vagrant
+
+source /etc/profile.d/rvm.sh
+
+rvm install 2.7.6
+
+rvm --default use 2.7.6
 
 # Installing kitchen
-bundle install --path vendor/bundle
 
+cd /vagrant
+
+bundle update --bundler
+
+bundle install
